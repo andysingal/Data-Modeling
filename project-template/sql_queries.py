473f8a0b112001f8a0e3d67a832806119517bb32
andysingal <andysingal@gmail.com>
2021-05-11 14:28:40 +0900

    update

--- project-template/sql_queries.py
+++ project-template/sql_queries.py
@@ -1,6 +1,6 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS song"
 artist_table_drop = "DROP TABLE IF EXISTS artist"
