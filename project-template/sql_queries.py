2a82862d73fa7df1e877159f660dcc6316001b64
andysingal <andysingal@gmail.com>
2021-05-11 10:43:14 +0900

    update

--- project-template/sql_queries.py
+++ project-template/sql_queries.py
@@ -33,10 +33,10 @@ CREATE TABLE songplays(
 user_table_create = ("""
 CREATE TABLE users 
     (
-        user_id    varchar PRIMARY KEY,\
-        first_name varchar,\
-        last_name  varchar,\ 
-        gender     varchar,\
+        user_id    varchar PRIMARY KEY,
+        first_name varchar,
+        last_name  varchar,
+        gender     varchar,
         level      varchar
     )
 """)
@@ -48,10 +48,10 @@ song_table_create = ("""
 
 CREATE TABLE songs 
     (
-        song_id   varchar PRIMARY KEY,\
-        title     varchar,\
-        artist_id varchar,\
-        year      int,\
+        song_id   varchar PRIMARY KEY,
+        title     varchar,
+        artist_id varchar,
+        year      int,
         duration  float
     )
 """)
@@ -59,10 +59,10 @@ CREATE TABLE songs
 artist_table_create = ("""
 CREATE TABLE artists 
     (
-        artist_id varchar PRIMARY KEY,\
-        name      varchar,\
-        location  varchar,\
-        latitude  varchar,\ 
+        artist_id varchar PRIMARY KEY,
+        name      varchar,
+        location  varchar,
+        latitude  varchar,
         longitude varchar
     )
 
@@ -84,31 +84,64 @@ CREATE TABLE time
 # INSERT RECORDS
 
 songplay_table_insert = (""" INSERT INTO songplays 
-                             (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
-                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) 
-                             ON CONFLICT (songplay_id) DO UPDATE SET
-                             (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) = 
-                             (EXCLUDED.start_time, EXCLUDED.user_id, EXCLUDED.level, EXCLUDED.song_id, EXCLUDED.artist_id, EXCLUDED.session_id,
+                             (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)  
+                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)  
+                             ON CONFLICT (songplay_id) DO UPDATE SET  
+                             (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) =   
+                             (EXCLUDED.start_time, EXCLUDED.user_id, EXCLUDED.level, EXCLUDED.song_id, EXCLUDED.artist_id, EXCLUDED.session_id, 
                               EXCLUDED.location, EXCLUDED.user_agent)
 
 """)
 
-user_table_insert = ("""
+user_table_insert = (""" INSERT INTO users 
+                         (user_id, first_name, last_name, gender, level) 
+                         VALUES (%s, %s, %s, %s, %s) 
+                         ON CONFLICT (user_id) DO UPDATE SET 
+                         (gender, level) = (
+                         CASE WHEN users.gender != EXCLUDED.gender 
+                         THEN users.gender || ';' || EXCLUDED.gender ELSE users.gender END, 
+                         CASE WHEN users.level != EXCLUDED.level
+                         THEN users.level || ';' || EXCLUDED.level ELSE users.level END)
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) 
+                         VALUES (%s, %s, %s, %s, %s) 
+                         ON CONFLICT (song_id) DO UPDATE SET 
+                         (title, artist_id, year, duration) = (EXCLUDED.title, EXCLUDED.artist_id, EXCLUDED.year, EXCLUDED.duration)
+
+
 """)
 
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) 
+                           VALUES (%s, %s, %s, %s, %s) 
+                           ON CONFLICT (artist_id) DO UPDATE SET 
+                           (name, location, latitude, longitude) = 
+                           (CASE WHEN artists.name != EXCLUDED.name 
+                            THEN artists.name || ';' || EXCLUDED.name ELSE artists.name END,
+                            CASE WHEN artists.location != EXCLUDED.location
+                            THEN artists.location ||';'|| EXCLUDED.location ELSE artists.location END, 
+                            CASE WHEN artists.latitude != EXCLUDED.latitude
+                            THEN artists.latitude ||';'|| EXCLUDED.latitude ELSE artists.latitude END,
+                            CASE WHEN artists.longitude != EXCLUDED.longitude
+                            THEN artists.longitude ||';'|| EXCLUDED.longitude ELSE artists.longitude END)
+
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time 
+                         (start_time, hour, day, week, month, year, weekday) 
+                         VALUES (%s, %s, %s, %s, %s, %s, %s) 
+                         ON CONFLICT (start_time) DO UPDATE SET
+                         (hour, day, week, month, year, weekday) = (EXCLUDED.hour, EXCLUDED.day, EXCLUDED.week, EXCLUDED.month, EXCLUDED.year, EXCLUDED.weekday)
 """)
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT songs.artist_id, songs.song_id FROM songs 
+                   JOIN artists ON songs.artist_id = artists.artist_id 
+                   WHERE songs.title = %s AND
+                   artists.name = %s AND
+                   songs.duration = %s
 """)
 
 # QUERY LISTS
